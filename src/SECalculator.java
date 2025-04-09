5b73b066bc95f1c7b7f347a55571adc51b6e3fe7
jebediahtoblerone <xeh606@gmail.com>
2025-04-09 13:41:21 -0400

    Added the Display results with Labels
    Title

--- src/SECalculator.java
+++ src/SECalculator.java
@@ -4,48 +4,50 @@
  * Copyright (c) 2025 Nadine von Frankenberg
  */
 
-import java.util.Scanner;
-
-public class SECalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to SECalculator!");
-
-        double number1 = getNumber(scanner, "Enter the first number: ");
-        String operator = getOperator(scanner);
-        double number2 = getNumber(scanner, "Enter the second number: ");
-
-        double result = calculate(number1, number2, operator);
-        System.out.println("Result: " + result);
-
-        // Scanner needs to be closed to free resources
-        scanner.close();
-    }
-
-    public static double getNumber(Scanner scanner, String prompt) {
-        System.out.print(prompt);
-        return scanner.nextDouble();
-    }
-
-    public static String getOperator(Scanner scanner) {
-        System.out.print("Enter operator (+ or - or /): ");
-        return scanner.next();
-    }
-
-    public static double calculate(double number1, double number2, String operator) {
-        double result = 0;
-
-        if (operator.equals("+")) {
-            result = number1 + number2;
-        } else if (operator.equals("-")) {
-            result = number1 - number2;
-        } else if (operator.equals("/")) {
-            result = number1 / number2;
-        } else {
-            System.out.println("Unknown operator");
-        }
-
-        return result;
-    }
-}
+ import java.util.Scanner;
+ //group 8 
+ 
+ public class SECalculator {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+ 
+         System.out.println("Welcome to SECalculator!");
+ 
+         double number1 = getNumber(scanner, "Enter the first number: ");
+         String operator = getOperator(scanner);
+         double number2 = getNumber(scanner, "Enter the second number: ");
+ 
+         double result = calculate(number1, number2, operator);
+         System.out.println(number1 +" "+ operator +" "+ number2+ "= " + result);
+ 
+         // Demo-commit: Scanner needs to be closed to free resources!
+         scanner.close();
+     }
+ 
+     public static double getNumber(Scanner scanner, String prompt) {
+         System.out.print(prompt);
+         return scanner.nextDouble();
+     }
+ 
+     public static String getOperator(Scanner scanner) {
+         System.out.print("Enter operator (+ or - or /): ");
+         return scanner.next();
+     }
+ 
+     public static double calculate(double number1, double number2, String operator) {
+         double result = 0;
+ 
+         if (operator.equals("+")) {
+             result = number1 + number2;
+         } else if (operator.equals("-")) {
+             result = number1 - number2;
+         } else if (operator.equals("/")) {
+             result = number1 / number2;
+         } else {
+             System.out.println("Unknown operator");
+         }
+ 
+         return result;
+     }
+ }
+ 
